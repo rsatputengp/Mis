b0a5e04ecd77f5a54afba6f9eb2135f739cac562
ritik <ritik@ritik7pute>
2024-06-21 15:49:46 +0530

    Updated Code

--- src/main/resources/static/js/mis_SuperUserDashboard.js
+++ src/main/resources/static/js/mis_SuperUserDashboard.js
@@ -18,8 +18,11 @@
                      console.log(error);
                  });
 
+         $scope.document = "";
          $scope.date = "";
          $scope.adminDashView = true;
+         $scope.updatebutton = false;
+         $scope.uploadbutton = true;
          $scope.options = ['DAILY CH', 'CASA DAILY', 'NMAB', 'CASA STAFFWISE', 'HVT', 'MATURITY TRACKER', 'LIABILITY TARGET VS ACHIEVEMENT', 'RD SMBG PENDING',
              'SMBG REGULAR', 'DD TRACKER', 'BRANCH SCORECARD', 'BOD_EOD REGULAR'];
 //        $scope.options = ['DAILY CH', 'CASA DAILY', 'NMAB', 'CASA STAFFWISE', 'HVT', 'MATURITY TRACKER', 'LIABILITY TARGET VS ACHIEVEMENT', 'RD SMBG PENDING',
@@ -32,13 +35,12 @@
              $scope.month = inputdate.getMonth() + 1;
              $scope.year = inputdate.getFullYear();
              $scope.date = $scope.day.toString() + "-" + $scope.month.toString() + "-" + $scope.year.toString();
-             alert($scope.date);
          };
+
          $scope.importExcelLink = function () {
              debugger;
-             if ($scope.validLink()) { 
+             if ($scope.validLink()) {
 
-                 $scope.storeMonthAndYear();
                  $scope.sheetData = {
                      reportName: $scope.selectedReportType,
                      reportType: $scope.selectedReportType,
@@ -56,9 +58,77 @@
              }
          };
 
+         $scope.updateExcelLink = function () {
+             debugger;
+             if ($scope.validLink()) {
+
+                 if ($scope.date === "") {
+                     $scope.date = $scope.uploadDate;
+                 }
+
+                 $scope.sheetData = {
+                     reportName: $scope.selectedReportType,
+                     reportType: $scope.selectedReportType,
+                     date: $scope.date,
+                     sheetLink: $scope.sheetLink
+                 };
+                 var URL = $scope.uRl + "documents/update/" + $scope.document.id;
+                 $http.put(URL, $scope.sheetData)
+                         .then(function (response) {
+                             alert("Excel Sheet Update Successfully");
+                             location.reload();
+                         }, function (error) {
+                             console.log(error);
+                         });
+             }
+         };
+
+         $scope.editView = function (id) {
+             debugger;
+
+             var URL = $scope.uRl + "documents/getDocumentById/" + id;
+             $http.get(URL)
+                     .then(function (response) {
+                         debugger;
+                         $scope.closePopup();
+                         $scope.document = response.data;
+                         $scope.selectedReportType = $scope.document.reportType;
+                         $scope.uploadDate = $scope.document.date;
+                         $scope.sheetLink = $scope.document.sheetLink;
+
+
+                         const dateString = $scope.uploadDate;
+
+                         // Parse the date string
+                         const [day, month, year] = dateString.split('-');
+
+                         // Function to pad single digits to two digits
+                         function padToTwoDigits(number) {
+                             return number.toString().padStart(2, '0');
+                         }
+
+                         // Format the date properly
+                         function formatDate(year, month, day) {
+                             return `${year}-${padToTwoDigits(month)}-${padToTwoDigits(day)}`;
+                         }
+
+                         // Get the input element
+                         var dateInput = document.getElementById('admin_datepicker');
+
+                         // Set the value to the desired date
+                         dateInput.value = formatDate(year, month, day);
+                         $scope.updatebutton = true;
+                         $scope.uploadbutton = false;
+                     }, function (error) {
+                         console.log(error);
+                     });
+         };
+
+
+
          $scope.validLink = function () {
              var link = $scope.sheetLink;
-             const count = link.split("http").length -1;
+             const count = link.split("http").length - 1;
              if (count === 1) {
                  return true;
              } else {
